5fa8d64073d4769d5164c4af13c6c6c6c0ac7586
Colton Belfils <cb6619@gmail.com>
2018-08-27 08:08:42 -0700

    yet another commit

--- src/functionParts/OpperationPart.java
+++ src/functionParts/OpperationPart.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package functionParts;
 
 /**
  *
  * @author CJDB
  */
-public class OpperationPart {
+public abstract class OpperationPart {
     
 }
